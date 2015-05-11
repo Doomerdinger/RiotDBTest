5f25ea3ad27366b91823e475895a03d6be4cb4cf
Doomerdinger <alectief@gmail.com>
2015-05-11 01:27:42 -0400

    Did a bunch of cool stuff. (Finished populate Rune)

--- DBTestForm/MainWindow.Designer.cs
+++ DBTestForm/MainWindow.Designer.cs
@@ -28,89 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.AddRuneButton = new System.Windows.Forms.Button();
-            this.runeIDTextbox = new System.Windows.Forms.TextBox();
-            this.runeNumTextbox = new System.Windows.Forms.TextBox();
-            this.slotIDTextbox = new System.Windows.Forms.TextBox();
-            this.runeIDLabel = new System.Windows.Forms.Label();
-            this.runeNumLabel = new System.Windows.Forms.Label();
-            this.slotIDLabel = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SummonerNameTextBox = new System.Windows.Forms.TextBox();
+            this.RegionCombo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // AddRuneButton
+            // SearchButton
             // 
-            this.AddRuneButton.Location = new System.Drawing.Point(166, 179);
-            this.AddRuneButton.Name = "AddRuneButton";
-            this.AddRuneButton.Size = new System.Drawing.Size(75, 23);
-            this.AddRuneButton.TabIndex = 0;
-            this.AddRuneButton.Text = "Add Rune";
-            this.AddRuneButton.UseVisualStyleBackColor = true;
-            this.AddRuneButton.Click += new System.EventHandler(this.AddRuneButton_Click);
+            this.SearchButton.Location = new System.Drawing.Point(12, 65);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(130, 23);
+            this.SearchButton.TabIndex = 7;
+            this.SearchButton.Text = "Search Summoner";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
             // 
-            // runeIDTextbox
+            // SummonerNameTextBox
             // 
-            this.runeIDTextbox.Location = new System.Drawing.Point(76, 12);
-            this.runeIDTextbox.MaxLength = 5;
-            this.runeIDTextbox.Name = "runeIDTextbox";
-            this.runeIDTextbox.Size = new System.Drawing.Size(100, 20);
-            this.runeIDTextbox.TabIndex = 1;
+            this.SummonerNameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.SummonerNameTextBox.MaxLength = 20;
+            this.SummonerNameTextBox.Name = "SummonerNameTextBox";
+            this.SummonerNameTextBox.Size = new System.Drawing.Size(130, 20);
+            this.SummonerNameTextBox.TabIndex = 8;
             // 
-            // runeNumTextbox
+            // RegionCombo
             // 
-            this.runeNumTextbox.Location = new System.Drawing.Point(76, 66);
-            this.runeNumTextbox.MaxLength = 5;
-            this.runeNumTextbox.Name = "runeNumTextbox";
-            this.runeNumTextbox.Size = new System.Drawing.Size(100, 20);
-            this.runeNumTextbox.TabIndex = 2;
+            this.RegionCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.RegionCombo.FormattingEnabled = true;
+            this.RegionCombo.Items.AddRange(new object[] {
+            "na",
+            "euw"});
+            this.RegionCombo.Location = new System.Drawing.Point(148, 11);
+            this.RegionCombo.Name = "RegionCombo";
+            this.RegionCombo.Size = new System.Drawing.Size(121, 21);
+            this.RegionCombo.TabIndex = 9;
             // 
-            // slotIDTextbox
-            // 
-            this.slotIDTextbox.Location = new System.Drawing.Point(76, 115);
-            this.slotIDTextbox.MaxLength = 3;
-            this.slotIDTextbox.Name = "slotIDTextbox";
-            this.slotIDTextbox.Size = new System.Drawing.Size(100, 20);
-            this.slotIDTextbox.TabIndex = 3;
-            // 
-            // runeIDLabel
-            // 
-            this.runeIDLabel.AutoSize = true;
-            this.runeIDLabel.Location = new System.Drawing.Point(27, 19);
-            this.runeIDLabel.Name = "runeIDLabel";
-            this.runeIDLabel.Size = new System.Drawing.Size(39, 13);
-            this.runeIDLabel.TabIndex = 4;
-            this.runeIDLabel.Text = "runeID";
-            // 
-            // runeNumLabel
-            // 
-            this.runeNumLabel.AutoSize = true;
-            this.runeNumLabel.Location = new System.Drawing.Point(27, 72);
-            this.runeNumLabel.Name = "runeNumLabel";
-            this.runeNumLabel.Size = new System.Drawing.Size(50, 13);
-            this.runeNumLabel.TabIndex = 5;
-            this.runeNumLabel.Text = "runeNum";
-            // 
-            // slotIDLabel
-            // 
-            this.slotIDLabel.AutoSize = true;
-            this.slotIDLabel.Location = new System.Drawing.Point(27, 122);
-            this.slotIDLabel.Name = "slotIDLabel";
-            this.slotIDLabel.Size = new System.Drawing.Size(34, 13);
-            this.slotIDLabel.TabIndex = 6;
-            this.slotIDLabel.Text = "slotID";
-            // 
-            // Form1
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.slotIDLabel);
-            this.Controls.Add(this.runeNumLabel);
-            this.Controls.Add(this.runeIDLabel);
-            this.Controls.Add(this.slotIDTextbox);
-            this.Controls.Add(this.runeNumTextbox);
-            this.Controls.Add(this.runeIDTextbox);
-            this.Controls.Add(this.AddRuneButton);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(280, 120);
+            this.Controls.Add(this.RegionCombo);
+            this.Controls.Add(this.SummonerNameTextBox);
+            this.Controls.Add(this.SearchButton);
+            this.Name = "MainWindow";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,13 +80,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button AddRuneButton;
-        private System.Windows.Forms.TextBox runeIDTextbox;
-        private System.Windows.Forms.TextBox runeNumTextbox;
-        private System.Windows.Forms.TextBox slotIDTextbox;
-        private System.Windows.Forms.Label runeIDLabel;
-        private System.Windows.Forms.Label runeNumLabel;
-        private System.Windows.Forms.Label slotIDLabel;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox SummonerNameTextBox;
+        private System.Windows.Forms.ComboBox RegionCombo;
     }
 }
 
--- DBTestForm/MainWindow.cs
+++ DBTestForm/MainWindow.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RiotSharp;
 
 namespace DBTestForm
 {
@@ -18,74 +19,243 @@ namespace DBTestForm
             InitializeComponent();
         }
 
-        private void AddRuneButton_Click(object sender, EventArgs e)
+        private void SearchButton_Click(object sender, EventArgs e)
         {
-            //Data Source=titan.csse.rose-hulman.edu;Initial Catalog=LeagueStats;Persist Security Info=True;User ID=tiefenaw;Password=***********
-            String conString = "Data Source=titan.csse.rose-hulman.edu;Initial Catalog=LeagueStats;Persist Security Info=True;User ID=tiefenaw;Password=Loonytoons0";
-            using (SqlConnection openCon = new SqlConnection(conString))
+            var api = RiotApi.GetInstance("efd26477-6526-4a14-afa2-79d0b8a571ea");
+            String summName = this.SummonerNameTextBox.Text;
+            //var staticAPI = StaticRiotApi.GetInstance("efd26477-6526-4a14-afa2-79d0b8a571ea");
+            #region OLD CODE TO POPULATE RUNE
+            //var runes = staticAPI.GetRunes(RiotSharp.Region.na, RiotSharp.StaticDataEndpoint.RuneData.stats);
+            
+            //var runes2 = staticAPI.GetRune()
+            //List<String> runeNames = new List<String>();
+            //List<String> runeTypes = new List<String>();
+            //int maxLength = 0;
+            //String maxName = "";
+            //String maxDesc = "";
+            //foreach(var s in runes.Runes.Values)
+            //{
+            //    if (runeNames.Contains(s.Name) == false)
+            //    {
+            //        Console.WriteLine("NAME: " + s.Name);
+            //        if (maxLength < s.Name.Length)
+            //        {
+            //            maxLength = s.Name.Length;
+            //            maxName = s.Name;
+            //        }
+            //        if (maxDesc.Length < s.Description.Length)
+            //            maxDesc = s.Description;
+            //        runeNames.Add(s.Name);
+            //    }
+
+            //    if (runeTypes.Contains(s.Metadata.Type) == false)
+            //    {
+            //        //Console.WriteLine("NAME: " + s.Name);
+            //        runeTypes.Add(s.Metadata.Type);
+            //    }
+            //    //Console.WriteLine(s.Name);
+            //}
+            //foreach(var s in runeTypes)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //Console.WriteLine("MAX LENGTH: " + maxLength);
+            //Console.WriteLine("MAX NAME: " + maxName);
+            //Console.WriteLine("MAX DESC L: " + maxDesc.Length);
+            //Console.WriteLine("MAX DESC: " + maxDesc);
+
+            /*try
             {
-                string addRuneString = " INSERT INTO Rune (runeID,runeNum,slotID) VALUES (@runeID,@runeNum,@slotID)";
-                using(SqlCommand addRune = new SqlCommand())
+                //RiotSharp.SummonerEndpoint.Summoner sum = api.GetSummoner(RiotSharp.Region.na, summName);
+                //Console.WriteLine("REGION: " + sum.Region);
+                String conString = "Data Source=titan.csse.rose-hulman.edu;Initial Catalog=LeagueStats;Persist Security Info=True;User ID=tiefenaw;Password=Loonytoons0";
+                using (SqlConnection openCon = new SqlConnection(conString))
                 {
-                    addRune.CommandText = "SET IDENTITY_INSERT Rune ON";
-                    addRune.CommandText += addRuneString;
-                    addRune.Connection = openCon;
-                    //queryAddRune.Parameters.Add("@runeID", SqlDbType.VarChar, 30).Value = name;
-                    int runeID = Convert.ToInt16(runeIDTextbox.Text);
-                    if (runeID < 0)
-                        return;
-                    addRune.Parameters.AddWithValue("@runeID", runeID);
+                    string addRuneString = " INSERT INTO RuneInfo (runeNum, description, tier, type, name) VALUES (@runeNum, @description, @tier, @type, @name)";
+                    using (SqlCommand addSummoner = new SqlCommand())
+                    {
+                        //addSummoner.CommandText = "SET IDENTITY_INSERT Rune ON";
+                        //addSummoner.Connection = openCon;
+                        //addSummoner.CommandText = "add_or_update_summoner";
+                        //addSummoner.CommandType = CommandType.StoredProcedure;
 
-                    int runeNum = Convert.ToInt16(runeNumTextbox.Text);
-                    if (runeNum < 0)
-                        return;
-                    addRune.Parameters.AddWithValue("@runeNum", runeNum);
+                        //SqlParameter param = new SqlParameter("@summonerID", sum.Id);
+                        //addSummoner.Parameters.Add(param);
 
-                    int slotID = Convert.ToInt16(slotIDTextbox.Text);
-                    if (slotID > 255 || slotID < 0)
-                        return;
-                    Byte slotIDByte = Convert.ToByte(slotID);
-                    addRune.Parameters.AddWithValue("@slotID", slotID);
-                    openCon.Open();
-                    addRune.ExecuteNonQuery();
-                    openCon.Close();
+                        //param = new SqlParameter("@region", sum.Region.ToString());
+                        //addSummoner.Parameters.Add(param);
+
+                        //param = new SqlParameter("@profileIconID", sum.ProfileIconId);
+                        //addSummoner.Parameters.Add(param);
+
+                        //param = new SqlParameter("@summonerLevel", sum.Level);
+                        //addSummoner.Parameters.Add(param);
+
+                        //param = new SqlParameter("@summonerName", sum.Name);
+                        //addSummoner.Parameters.Add(param);
+                        addSummoner.CommandText += addRuneString;
+                        addSummoner.Connection = openCon;
+                        openCon.Open();
+                        foreach (var s in runes.Runes.Values)
+                        {
+                            addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            addSummoner.Parameters.Add("@description", s.Description);
+                            addSummoner.Parameters.Add("@tier", Convert.ToByte(s.Metadata.Tier));
+                            addSummoner.Parameters.Add("@type", s.Metadata.Type);
+                            addSummoner.Parameters.Add("@name", s.Name);
+                            addSummoner.ExecuteNonQuery();
+                            addSummoner.Parameters.Clear();
+                            //if(s.Metadata.Type.CompareTo("black") == 0)
+                            //{
+                            //    for(int i = 28; i <= 30; i++)
+                            //    {
+                            //        addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            //        addSummoner.Parameters.Add("@slotID", Convert.ToByte(i));
+                            //        addSummoner.Parameters.Add("@description", s.Description);
+                            //        addSummoner.Parameters.Add("@tier", Convert.ToByte(s.Metadata.Tier));
+                            //        addSummoner.Parameters.Add("@type", s.Metadata.Type);
+                            //        addSummoner.Parameters.Add("@name", s.Name);
+                            //        addSummoner.ExecuteNonQuery();
+                            //        addSummoner.Parameters.Clear();
+                            //    }
+                            //}
+                            //else if(s.Metadata.Type.CompareTo("red") == 0)
+                            //{
+                            //    for(int i = 1; i <= 9; i++)
+                            //    {
+                            //        addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            //        addSummoner.Parameters.Add("@slotID", Convert.ToByte(i));
+                            //        addSummoner.Parameters.Add("@description", s.Description);
+                            //        addSummoner.Parameters.Add("@tier", Convert.ToByte(s.Metadata.Tier));
+                            //        addSummoner.Parameters.Add("@type", s.Metadata.Type);
+                            //        addSummoner.Parameters.Add("@name", s.Name);
+                            //        addSummoner.ExecuteNonQuery();
+                            //        addSummoner.Parameters.Clear();
+                            //    }
+                            //} else if(s.Metadata.Type.CompareTo("yellow") == 0)
+                            //{
+                            //    for(int i = 10; i <= 18; i++)
+                            //    {
+                            //        addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            //        addSummoner.Parameters.Add("@slotID", Convert.ToByte(i));
+                            //        addSummoner.Parameters.Add("@description", s.Description);
+                            //        addSummoner.Parameters.Add("@tier", Convert.ToByte(s.Metadata.Tier));
+                            //        addSummoner.Parameters.Add("@type", s.Metadata.Type);
+                            //        addSummoner.Parameters.Add("@name", s.Name);
+                            //        addSummoner.ExecuteNonQuery();
+                            //        addSummoner.Parameters.Clear();
+                            //    }
+                            //} else if(s.Metadata.Type.CompareTo("blue") == 0)
+                            //{
+                            //    for(int i = 19; i <= 27; i++)
+                            //    {
+                            //        addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            //        addSummoner.Parameters.Add("@slotID", Convert.ToByte(i));
+                            //        addSummoner.Parameters.Add("@description", s.Description);
+                            //        addSummoner.Parameters.Add("@tier", Convert.ToByte(s.Metadata.Tier));
+                            //        addSummoner.Parameters.Add("@type", s.Metadata.Type);
+                            //        addSummoner.Parameters.Add("@name", s.Name);
+                            //        addSummoner.ExecuteNonQuery();
+                            //        addSummoner.Parameters.Clear();
+                            //    }
+                            //}
+                            //addSummoner.Parameters.Add("@runeNum", Convert.ToInt16(s.Id));
+                            //addSummoner.Parameters.Add("@slotID", Convert.ToByte(s.));
+                            //addSummoner.Parameters.Add("@runeNum", SqlDbType.SmallInt).Value = Convert.ToInt16(s.Id);
+                            //int runeID = Convert.ToInt16(runeIDTextbox.Text);
+                            //if (runeNames.Contains(s.Name) == false)
+                            //{
+                            //    Console.WriteLine("NAME: " + s.Name);
+                            //    if (maxLength < s.Name.Length)
+                            //    {
+                            //        maxLength = s.Name.Length;
+                            //        maxName = s.Name;
+                            //    }
+                            //    if (maxDesc.Length < s.Description.Length)
+                            //        maxDesc = s.Description;
+                            //    runeNames.Add(s.Name);
+                            //}
+
+                            //if (runeTypes.Contains(s.Metadata.Type) == false)
+                            //{
+                            //    //Console.WriteLine("NAME: " + s.Name);
+                            //    runeTypes.Add(s.Metadata.Type);
+                            //}
+                            ////Console.WriteLine(s.Name);
+                        }
+                        //addSummoner.Parameters.Add("@runeNum", SqlDbType.VarChar, 30).Value = name;
+                        //int runeID = Convert.ToInt16(runeIDTextbox.Text);
+
+                        //addSummoner.Parameters.AddWithValue("@runeID", runeID);
+
+                        //int runeNum = Convert.ToInt16(runeNumTextbox.Text);
+                        //if (runeNum < 0)
+                        //    return;
+                        //addSummoner.Parameters.AddWithValue("@runeNum", runeNum);
+
+                        //int slotID = Convert.ToInt16(slotIDTextbox.Text);
+                        //if (slotID > 255 || slotID < 0)
+                        //    return;
+                        //Byte slotIDByte = Convert.ToByte(slotID);
+                        //addSummoner.Parameters.AddWithValue("@slotID", slotID);
+                        //openCon.Open();
+                        //addSummoner.ExecuteNonQuery();
+                        openCon.Close();
+                    }
                 }
             }
-
-
-            //The following is just some random code to do stored procedures.
-            /*using (SqlConnection openCon = new SqlConnection(conString))
+            catch (Exception cept)
             {
-                SqlCommand cmdNewCustomer = new SqlCommand("Sales.uspNewCustomer", openCon);
-                cmdNewCustomer.CommandType = CommandType.StoredProcedure;
-
-                cmdNewCustomer.Parameters.Add(new SqlParameter("@CustomerName", SqlDbType.NVarChar, 40));
-                cmdNewCustomer.Parameters["@CustomerName"].Value = "SomeName";
-
-                //NC-9 Add output parameter.
-                cmdNewCustomer.Parameters.Add(new SqlParameter("@CustomerID", SqlDbType.Int));
-                cmdNewCustomer.Parameters["@CustomerID"].Direction = ParameterDirection.Output;
-
+                throw (cept);
+            } */
+            #endregion
+            if (this.RegionCombo.Text == "na")
+            {
                 try
                 {
-                    //NC-11 Open the connection.
-                    openCon.Open();
+                    RiotSharp.SummonerEndpoint.Summoner sum = api.GetSummoner(RiotSharp.Region.na, summName);
+                    Console.WriteLine("REGION: " + sum.Region);
+                    String conString = "Data Source=titan.csse.rose-hulman.edu;Initial Catalog=LeagueStats;Persist Security Info=True;User ID=tiefenaw;Password=Loonytoons0";
+                    using (SqlConnection openCon = new SqlConnection(conString))
+                    {
+                        //string addRuneString = " INSERT INTO Rune (runeID,runeNum,slotID) VALUES (@runeID,@runeNum,@slotID)";
+                        using (SqlCommand addSummoner = new SqlCommand())
+                        {
+                            addSummoner.Connection = openCon;
+                            addSummoner.CommandText = "add_or_update_summoner";
+                            addSummoner.CommandType = CommandType.StoredProcedure;
 
-                    //NC-12 Run the stored procedure.
-                    cmdNewCustomer.ExecuteNonQuery();
+                            SqlParameter param = new SqlParameter("@summonerID", sum.Id);
+                            addSummoner.Parameters.Add(param);
 
-                    //NC-13 Customer ID is an IDENTITY value from the database. 
-                    this.parsedCustomerID = (int)cmdNewCustomer.Parameters["@CustomerID"].Value;
-                    this.txtCustomerID.Text = Convert.ToString(parsedCustomerID);
+                            param = new SqlParameter("@region", sum.Region.ToString());
+                            addSummoner.Parameters.Add(param);
 
-                }
-                catch
+                            param = new SqlParameter("@profileIconID", sum.ProfileIconId);
+                            addSummoner.Parameters.Add(param);
+
+                            param = new SqlParameter("@summonerLevel", sum.Level);
+                            addSummoner.Parameters.Add(param);
+
+                            param = new SqlParameter("@summonerName", sum.Name);
+                            addSummoner.Parameters.Add(param);
+                            //addSummoner.CommandText += addRuneString;
+
+                            openCon.Open();
+                            addSummoner.ExecuteNonQuery();
+                            openCon.Close();
+                        }
+                    }
+                } catch(Exception cept)
                 {
-                    //NC-14 A simple catch.
-
-                    MessageBox.Show("Customer ID was not returned. Account could not be created.");
+                    Console.WriteLine("ERROR");
                 }
-            } */
+            }
+            
+            //var me = EF.RiotApi.Client.RiotWeb.API.Summoner.GetSummoner("Doomerdinger");
+            //Console.WriteLine("Name:{0} ID:{1} Level:{2}", sum.Name, sum.Id, sum.Level);
+            //var me = EF.RiotApi.Client.RiotWeb.API.Summoner.GetSummoner("Dome");
+            //Console.WriteLine("{0}:{1}", me.Name, me.Id);
         }
     }
 }
